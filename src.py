fbf620d36c81478d6062d2aa7c41d192592e7e32
Ordep <58833700+SrStark666@users.noreply.github.com>
2023-01-05 21:09:17 -0300

    Add files via upload

---
+++ src.py
@@ -0,0 +1,59 @@
+from DoS.configs_dos import DenialOfService
+from Panel.configs_panel import Finder
+from Proxy.configs_proxy import Proxy
+from sys import argv
+from time import sleep
+import os
+
+class Commands:
+    __slots__ = ("objects_class", "verify_digit", "undefinied", "mode", "parameters", "colors")
+    
+    def __init__(self, mode: str) -> None:
+        self.parameters = {}
+        self.mode = mode
+        self.objects_class = {"denial": [DenialOfService(), DenialOfService().commands],
+                        "finder": [Finder(), Finder().commands], "proxy": [Proxy(), Proxy().commands],
+                        "comp": ["-h", "-r"]} ##Comandos de cada metodo
+        self.verify_digit = {mode: [i for i in argv if mode != "comp" and i in self.objects_class[mode][1]]} #Comandos digitados pelo usuário
+        self.undefinied = list(set([i for i in self.objects_class[mode] if mode != "comp" for i in self.objects_class[mode][1] if i not in self.verify_digit[mode]])) #Comandos que faltam atribuir
+        self.colors = {'purplenegr': '\033[1;35m', 'yellow': '\033[0;33m', 'red': '\033[0;31m',
+                    'rednegr': '\033[1;31m', 'green': '\033[0;32m', 'endcolor': '\033[m'}
+
+    def requirements(self) -> str: ##Install requirements
+        req = ["requests", "subprocess", "os",
+                "socket", "threading", "sys", "re"]
+
+        for i in req:
+            os.system(f"pip install {i}")
+            sleep(2)
+
+        print(f"Instalação concluída com sucesso!!")
+        sleep(2)
+        print("\x1b[H\x1b[2J\x1b[3J", end="") #Limpar a tela
+        for i in req:
+            print(f"Pacotes instalados: {i}")
+
+        return req
+
+    def filter_arguments(self) -> None:
+        if self.mode in ["comp"] and argv[2] == "-h":
+            print(f"""
+            Command lines
+                Digit [mode] -h for help menu               
+                Example: python3 Sunev.py -h denial
+                Install dependencies: python3 Sunev.py -d
+                Options menu: -denial | -finder | -proxy
+            """)
+        elif self.mode in self.objects_class and argv[2] == "-h":
+            self.objects_class[self.mode][0].help_()
+        elif self.undefinied:
+            print(f"{self.colors['rednegr']}Error undefined parameters:{self.colors['endcolor']} {(' ').join(self.undefinied)}")
+        elif self.mode in self.objects_class and argv[2] == "-r":
+            self.requirements()
+        elif self.verify_digit[self.mode] == self.objects_class[self.mode][1]:
+            for i in range(len(self.verify_digit[self.mode])):
+                def_parameters = self.verify_digit[self.mode][i]
+                self.parameters[def_parameters] = argv[int(argv.index(def_parameters)+1)]
+
+            list_param = list(self.parameters.values())
+            self.objects_class[self.mode][0].main(list_param) #Instancia da classe atual com o metodo main.
--- sunev_main.py
+++ sunev_main.py
@@ -1,13 +1,8 @@
-from DoS.configs_dos import DenialOfService
-from Panel.configs_panel import Finder
-from Proxy.configs_proxy import proxy_
-from time import sleep
+from src import Commands
 from sys import argv
-import socket
-import os
 
 class Start:
-    __slots__ = ("commands","undefinied", "arg_denial", "arg_finder", "colors", "arg_complement", "point", "modes_smallen", "arg_proxy", "proxy")
+    __slots__ = ("colors")
 
     def __init__(self) -> None:
         self.colors = {'purplenegr': '\033[1;35m',
@@ -17,156 +12,16 @@ class Start:
                 'green': '\033[0;32m',
                 'endcolor': '\033[m'}
 
-        self.commands = {"denial": ["-t", "-p", "-th", "-pkg", "-b"],
-                        "finder": ["-w", "-p"], 
-                        "proxy": ["-a"],
-                        "complement": ["-h"]}
-                        
-        self.arg_denial = [i for i in argv if i in self.commands["denial"]] #list: [str] -> [argv]
-        self.arg_finder = [i for i in argv if i in self.commands["finder"]]
-        self.arg_complement = [i for i in argv if i in self.commands["complement"]]
-        self.arg_proxy = [i for i in argv if i in self.commands["proxy"]]
-        self.undefinied = [] #list: [str] -> [argv not in self.commands]
-        self.modes_smallen = ["finder", "proxy"] #Modes with arguments less than 5
-        self.proxy = proxy_("1")
-
-    def help_all(self, mode) -> None: ##Panel for help users 
-        if "denial" in mode:
-            print(f"""
-        Command lines DoS:
-            {self.commands["complement"][0]} help to use
-            {self.commands["denial"][0]} for especifies of target
-            {self.commands["denial"][1]} for especifies of port
-            {self.commands["denial"][2]} number of threads
-            {self.commands["denial"][3]} number of packages for send
-            {self.commands["denial"][4]} for especifies of lenght bytes
-
-            Example: python3 Sunev.py -denial -t 127.0.0.1 -p 80 -th 5 -pkg 3 -b 1048
-                """)
-
-        elif "finder" in mode:
-            print(f"""
-        Command lines Finder:
-            {self.commands["complement"][0]} help to use
-            {self.commands["finder"][0]} Website target
-            {self.commands["finder"][1]} Protocol
-            
-            Example: python3 Sunev.py -finder -w www.google.com -p https
-                """)
-
-        elif "main" in mode:
-            print(f"""
-            Digit [mode] -h for help menu               
-            Example: python3 Sunev.py -h denial
-            Install dependencies: python3 Sunev.py -d
-            Options menu: -denial | -finder | -proxy
-            """)
-
-        elif "proxy" in mode:
-            print(f"""
-        Command Lines Proxy:
-            {self.commands['complement'][0]} help to use
-            {self.commands['proxy'][0]} name archive for save list proxy.txt [Optional]
-
-            Example: python3 sunev.py -proxy -a list_proxy.txt
-            """)
-
-    def requirements(self) -> str: ##Install requirements
-        req = ["requests", "subprocess", "os",
-                "socket", "threading", "sys", "re"]
-
-        for i in req:
-            os.system(f"pip install {i}")
-            sleep(2)
-
-        print(f"Instalação concluída com sucesso!!")
-        sleep(2)
-        print("\x1b[H\x1b[2J\x1b[3J", end="") #Limpar a tela
-        for i in req:
-            print(f"Pacotes instalados: {i}")
-
-        return req
-
-    def filter_arguments(self, mode: str, attr: str) -> None: #mode: str [-mode], attr: str[self.arg_denial]
-        if f"-{mode}" in argv[1]:
-            if len(argv) == 3 and "-h" in self.commands["complement"]:
-                self.help_all(f"{mode}")
-                exit()
-
-            elif mode not in self.modes_smallen and len(attr) >= 1 and len(attr) < 5:
-                for commands in self.commands[mode]:           
-                    if not commands in attr:      
-                        self.undefinied.append(commands)
-                print(f"Error undefined parameters: {(' ').join(self.undefinied)}")
-
-            elif attr == self.commands[mode]:
-                if mode == "denial":
-                    port = int(argv.index("-p")+1)
-                    threads = int(argv.index("-th")+1)
-                    packages = int(argv.index("-pkg")+1)
-                    byt = int(argv.index("-b")+1)
-
-                    params = {"Host": socket.gethostbyname(argv[3]),
-                                "Port": int(argv[port]), 
-                                "Threads": int(argv[threads]),
-                                "Packages": int(argv[packages]),
-                                "Bytes": int(argv[byt])}
-
-                    obj_class = DenialOfService(params["Host"], params["Port"])
-                    obj_class.multi(params["Threads"], params["Bytes"], params["Packages"])
-
-                elif mode == "finder":
-                    website = int(argv.index("-w")+1)
-                    protocol = int(argv.index("-p")+1)
-                    
-                    params = {"Website": str(argv[website]),
-                                "Protocol": str(argv[protocol])}
-                                
-                    obj_class = Finder()
-                    obj_class.main(params["Website"],
-                                    params["Protocol"])
-
-                elif mode == "proxy":
-                    params = {"Save": None}
-                    try:
-                        save = int(argv.index("-a"))
-                        if argv[save+1] != "1":
-                            params["Save"] = argv[save+1]
-                    except ValueError:
-                        params["Save"] = "1"
-                    proxy_(params["Save"])
-
     def main(self) -> None:
         try:
-            while 1:
-                if "-h" in argv[1]:
-                    self.help_all("main")
-                    break
-
-                elif "-proxy" in argv[1]:
-                    self.point = self.arg_proxy
-                    self.filter_arguments("proxy", self.point)
-                    break
-
-                elif len(argv) == 2 and argv[1] in ["-d"]:
-                    self.requirements()
-                    break
-
-                elif "-denial" in argv[1]:
-                    self.point = self.arg_denial
-                    self.filter_arguments("denial", self.point)
-                    break
-
-                elif "-finder" in argv[1]:
-                    self.point = self.arg_finder
-                    self.filter_arguments("finder", self.point)
-                    break
-
-                elif not len(self.arg_denial) and not len(self.arg_finder) and not len(self.arg_proxy) and not len(self.arg_complement):
-                    print("No parameter set, digit surnev.py -h for help.")
-                    break
-        except:
-            print("Parameter error, verify arguments")
+            obj_class = Commands(argv[1])
+            obj_class.filter_arguments()
+        except IndexError:
+            print(f"{self.colors['red']}No parameter was set\nDigit python3 sunev_main.py comp -h for help{self.colors['endcolor']}")
+        except KeyboardInterrupt:
+            print(f"{self.colors['red']}Program interrupt{self.colors['endcolor']}")
+        except KeyError:
+            print(f"{self.colors['rednegr']}Key ({argv[1]}) not found{self.colors['endcolor']}")
 
 obj_class = Start()
 obj_class.main()
