5d55a1ceb80f7d3fa182677e0ba8ed36819b92b5
Ordep <58833700+SrStark666@users.noreply.github.com>
2023-01-05 21:10:03 -0300

    Add files via upload

--- Panel/configs_panel.py
+++ Panel/configs_panel.py
@@ -1,7 +1,9 @@
+from time import sleep
+import sys
 import requests
 
 class Finder:
-    __slots__ = ("protocol", "colors", "valids")
+    __slots__ = ("protocol", "colors", "valids", "commands", "target")
 
     def __init__(self) -> None:
         self.colors = {'purplenegr': '\033[1;35m',
@@ -10,16 +12,28 @@ class Finder:
                 'rednegr': '\033[1;31m',
                 'green': '\033[0;32m',
                 'endcolor': '\033[m'}
+        self.commands = ["-w", "-p"]
         self.protocol = ""
         self.valids = [] #list: [str]
 
-    def main(self, site, protocol) -> None:
+    def help_(self) -> None:
+        print(f"""
+        Command lines Finder:
+            -h help to use
+            -w Website target
+            -p Protocol
+            
+            Example: python3 Sunev.py -finder -w www.google.com -p https
+                """)
+                
+    def main(self, list_args) -> None:
+        dict_args = {"Site": list_args[0], "Protocol": list_args[1]}
         while 1:
-            if not 'http' in protocol or not 'https' in protocol:
+            if not 'http' in dict_args["Protocol"] or not 'https' in dict_args["Protocol"]:
                 print(f"{self.colors['rednegr']}Without http or https{self.colors['endcolor']}")
                 break
 
-            address = (protocol + ":" + "//" + site + '/').strip('\n')
+            address = (dict_args["Protocol"] + ":" + "//" + dict_args["Site"] + '/').strip('\n')
             try:
                 wordlist = open(f'Panel/pages.txt', 'r+')
 
@@ -28,10 +42,10 @@ class Finder:
                     get = requests.get(link)
             
                     if get.status_code == 200:
-                        print(f"{link} {self.colors['green']}is accessible and potentially an admin panel{self.colors['endcolor']}")
+                        print(f"\n{link} {self.colors['green']}is accessible and potentially an admin panel{self.colors['endcolor']}")
                         self.valids.append(link)
                     else:
-                        print(f"{link} {self.colors['red']}error 404{self.colors['endcolor']}")
+                        sys.stdout.write(f"\r{link} {self.colors['red']}404{self.colors['endcolor']}")
             except KeyboardInterrupt:
                 print(f"{self.colors['rednegr']}Program interrupt!{self.colors['endcolor']}")
             except:
